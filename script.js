1456c42580f86885339aef98c968625644f3ec46
justin1045 <cstank786@gmail.com>
2025-02-22 23:36:35 +0530

    created simple dom project and added styling through js

---
+++ script.js
@@ -0,0 +1,20 @@
+const div = document.getElementById("container");
+
+div.style.backgroundColor="yellow"
+div.style.width="300px" 
+div.style.height="200px"
+// div.style.margin="20px"
+div.style.padding="10px"
+div.style.margin="200px auto"
+div.style.display="flex"
+div.style.alignItems="center"
+div.style.justifyContent="center"
+
+
+const para = document.querySelector("p");
+
+para.style.fontSize="18px"
+para.style.fontWeight="bold"
+para.style.color="blue"
+
+
